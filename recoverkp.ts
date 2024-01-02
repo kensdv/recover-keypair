d61c16c31cd10bff4461aaad935c29c38991443c
KeNs <108461458+kensdv@users.noreply.github.com>
2024-01-01 22:49:27 -0800

    Add files via upload

---
+++ recoverkp.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+
+const bs58 = require('bs58')
+
+const pk = 'your-private-key-here'
+const uint8bytes = bs58.decode(pk);
+
+const bytelist : number[] =[]
+for (let byte of uint8bytes){
+    bytelist.push(byte);
+}
+
+fs.writeFile("keypair.json", JSON.stringify(bytelist), (err) => {
+    if (err) { console.error(err); return; };
+    console.log("Key Pair file is created!");
+
+});
